4f5f750ee780416c153f1c0b324711cdd2e15745
Ilan Angeles Rodriguez <ilanangelesrodriguez@gmail.com>
2025-01-01 18:27:15 -0500

    feat: agregar configuracion de conexion a la bd

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 import express from 'express';
+import connectDB from "./src/config/connection.js";
 
-const app = express()
-const port = 3003
+const app = express();
+const port = process.env.PORT;
+
+connectDB()
+    .then(() => {
+      app.listen(port, () => {
+        console.log(`Aplicación escuchando en el puerto ${port}`);
+      });
+    })
+    .catch(() => {
+        process.exit(1); // Salir del proceso con fallo
+    });
 
 app.get('/', (req, res) => {
   res.send('Hola Mundo!')
-})
-
-app.listen(port, () => {
-  console.log(`Aplicación escuchando en el puerto ${port}`)
 })
