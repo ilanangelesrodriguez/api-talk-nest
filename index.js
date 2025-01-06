afe17526f2e032789cd5a1809281ba26744e0c99
Ilan Angeles Rodriguez <ilanangelesrodriguez@gmail.com>
2025-01-06 09:53:31 -0500

    chore: refactorizar carpetas

--- index.js
+++ index.js
@@ -2,28 +2,14 @@ import express from 'express';
 import cors from 'cors';
 import connectDB from "./src/config/connection.js";
 import corsOptions from "./src/config/cors.js";
-import authMiddleware from "./src/infrastructure/middleware/authMiddleware.js";
-import {ApolloServer} from "apollo-server-express";
-import typeDefs from "./src/infrastructure/graphql/typeDefs.js";
-import resolvers from "./src/infrastructure/graphql/resolvers.js";
 
 const app = express();
 const port = process.env.PORT;
 
 app.use(cors(corsOptions));
 
-// Middleware de autenticación
-app.use(authMiddleware);
-
 connectDB()
     .then(async () => {
-        // Configura Apollo Server
-        const server = new ApolloServer({ typeDefs, resolvers });
-        await server.start();
-        server.applyMiddleware({ app });
-
-
-
         app.listen(port, () => {
         console.log(`Aplicación escuchando en el puerto ${port}`);
       });
